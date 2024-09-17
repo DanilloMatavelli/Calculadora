02fb549a04b7194d7a2a08849d780e6461c0184a
Danillo Matavelli <danillomatavelli26@gmail.com>
2024-09-17 14:59:41 -0300

    MODIFY: Desing de cor da tabela

--- Progamando.cs
+++ Progamando.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,21 @@ namespace Calculadora
         public Progamando()
         {
             InitializeComponent();
+        }
+
+        private void Progamando_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
